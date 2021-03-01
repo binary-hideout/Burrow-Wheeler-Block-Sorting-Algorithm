9068a5ee135072e1a056d50813cf52d60baed05e
Diegobr21 <jdiegobrana@gmail.com>
2021-03-01 10:53:29 -0600

    Block-sorting-forward transformation completed, parameters for the block-sorting-reverse transformation ready

--- BWBS.py
+++ BWBS.py
@@ -1,4 +1,4 @@
-from itertools import combinations, permutations
+#from itertools import combinations, permutations
 
 def all_string_permutations(char:str):
     """
@@ -12,23 +12,42 @@ def all_string_permutations(char:str):
         raise ValueError("La string no puede estar vacía")
     return [char[i:] + char[:i] for i in range(len(char))]
 
-print(all_string_permutations('hello'))
 
 def block_sorting_forward(char:str):
     """
     ? :param char: La string con la que se hará la matriz y se obtendrá el resultado para después aplicar
     ? 'block-sorting-reverse-transformation
+    ? :return: tupla con la combinación del último caracter de cada fila y el indice de la string original
     """
     if not isinstance(char, str):
         raise TypeError("El parametro debe ser una string")
     if not char:
         raise ValueError("La string no puede estar vacía")
 
-def block_sorting_reverse_transformation(char:str):
+    cyclic_permutations = all_string_permutations(char)
+    print(f'permutaciones de la string "{char}":')
+    print(cyclic_permutations, '\n')
+    cyclic_permutations.sort()
+    print(f'permutaciones de la string "{char}" ordenadas alfabeticamente:')
+    print(cyclic_permutations)
+
+    combination = ''
+    for perm in cyclic_permutations:
+        combination += perm[-1]
+    original_word_index = cyclic_permutations.index(char)
+
+    return combination, original_word_index
+
+
+def block_sorting_reverse_transformation(char:str, index:int):
     """
-    ? :param matrix: El resultado de block-sorting-forward
+    ? :param char: La string resultante de bs-forward
+    ? :param index: El indíce de la string original
     """
     if not isinstance(char, str):
         raise TypeError("El parametro debe ser una string")
     if not char:
         raise ValueError("La string no puede estar vacía")
+
+
+print(block_sorting_forward('hello!'))
